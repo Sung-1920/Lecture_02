9087b2cb005f7653f9ef62d6c27f00a90ce70922
Sung-1920 <giorgifailodze059@gmail.com>
2026-03-16 11:43:28 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+// სავარჟიშო #1
+function roundUp(num) {
+    return Math.ceil(num);
+}
+
+console.log(roundUp(2.8))
+console.log(roundUp(15.8))
+console.log(roundUp(25.8))
+
+// სავარჟიშო #2
+
+function roundDown(num) {
+    return Math.floor(num);
+}
+
+console.log(roundDown(10.3))
+console.log(roundDown(19.20))
+console.log(roundDown(20.5))
+
+// სავარჟიშო #3
+
+function roundNumber(num) {
+    return Math.round(num);
+}
+
+console.log(roundNumber(5.3))
+console.log(roundNumber(19.99))
+console.log(roundNumber(20.5))
+
+// სავარჟიშო #4
+
